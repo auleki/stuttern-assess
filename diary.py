26a9a2d156083efb4ae8b5f7fe07ef7362409179
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2024-03-11 09:01:08 +0100

    add comments for clarity

--- diary.py
+++ diary.py
@@ -1,18 +1,15 @@
 import sys
 
-# Program would ask for a contact's number and their name
-# These names would be stored within a list and would be sorted
-
-# Program can start by asking one of two questions
-# New contact OR View All Contacts
-
+# list that holds all contacts
 all_contacts = []
+
 is_active = True
 
 all = "all"
 new = "new"
 close = "close"
 
+# method to handle adding contact to list and validating user entries
 def add_contact():
     name = input("New contact's full name\n> ")
     while (name == ''):
@@ -31,6 +28,7 @@ def add_contact():
     print(f"{name} added to diary")
     is_active = True
 
+# Method to show and format all contacts
 def show_contacts(contacts):
     if (len(contacts) == 0):
         print('No contacts found 😭')
@@ -40,10 +38,12 @@ def show_contacts(contacts):
         print(f"{contact['name']} | {contact['number']}\n")
     is_active = True
 
-
+# Method to end program and exit script instance
 def end_program():
-    is_active = False
+    print('Thanks for using AuDairies 👋🏼🌚')
+    sys.exit()
 
+# Loop that controls the program flow based on what the user selects
 while (is_active):
     user_choice = input("Type (ALL)📱 to Show Contacts or (NEW)⨁ to create a new contact or (CLOSE)🔚 to close the program\n> ")
 
@@ -52,5 +52,4 @@ while (is_active):
     if (user_choice.lower() == new):
         add_contact()
     if (user_choice.lower() == close):
-        print('Thanks for using AuDairies 👋🏼🌚')
-        sys.exit()
+        end_program()
